1ec3b0a162cfb5257f5fc404de6ee53682421022
zzh <118289872+2376780283@users.noreply.github.com>
2025-07-25 12:39:30 +0800

    OptionsDialog：修复导航栏touch的string🫠🤗

--- gameui/OptionsDialog.cpp
+++ gameui/OptionsDialog.cpp
@@ -88,7 +88,7 @@ COptionsDialog::COptionsDialog(vgui::Panel *parent) : PropertyDialog(parent, "Op
 	AddPage(new COptionsSubMouse(this), "#GameUI_Mouse");
 
 #ifdef ANDROID
-	AddPage(new COptionsSubTouch(this), "Touch");
+	AddPage(new COptionsSubTouch(this), "#GameUI_Touch");
 #endif
 
 	m_pOptionsSubAudio = new COptionsSubAudio(this);
