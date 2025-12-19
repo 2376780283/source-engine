22e3f513e7e7f85a404ace0d38251679e4bfebda
2376780283 <2376780283@qq.com>
2025-12-19 18:14:29 +0800

    20251219: Fix Touch

--- gameui/OptionsDialog.cpp
+++ gameui/OptionsDialog.cpp
@@ -86,7 +86,7 @@ COptionsDialog::COptionsDialog(vgui::Panel *parent) : PropertyDialog(parent, "Op
 	AddPage(new COptionsSubKeyboard(this), "#GameUI_Keyboard");
 	AddPage(new COptionsSubMouse(this), "#GameUI_Mouse");
 #ifdef ANDROID
-	AddPage(new COptionsSubTouch(this), "#GameUI_Touch");
+	AddPage(new COptionsSubTouch(this), "Touch");
 #endif
 	m_pOptionsSubAudio = new COptionsSubAudio(this);
 	AddPage(m_pOptionsSubAudio, "#GameUI_Audio");
