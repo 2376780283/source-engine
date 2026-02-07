6b28e20006ee535ac573b0a65955c8b7dd464c18
2376780283 <2376780283@qq.com>
2026-02-07 18:04:53 +0800

    update For Sync

--- launcher/android/main.cpp
+++ launcher/android/main.cpp
@@ -29,19 +29,19 @@ int iLastArgs = 0;
 
 extern void InitCrashHandler();
 DLL_EXPORT int LauncherMain( int argc, char **argv ); // from launcher.cpp
-
-DLL_EXPORT int Java_com_valvesoftware_ValveActivity2_setenv(JNIEnv *jenv, jclass *jclass, jstring env, jstring value, jint over)
+// zzh.source.launcher.utils  GameBridge
+DLL_EXPORT int Java_zzh_source_launcher_utils_GameBridge_setenv(JNIEnv *jenv, jclass *jclass, jstring env, jstring value, jint over)
 {
-	Msg( "Java_com_valvesoftware_ValveActivity2_setenv %s=%s\n", jenv->GetStringUTFChars(env, NULL), jenv->GetStringUTFChars(value, NULL) );
+	Msg( "Java_zzh_source_launcher_utils_GameBridge_setenv %s=%s\n", jenv->GetStringUTFChars(env, NULL), jenv->GetStringUTFChars(value, NULL) );
 	return setenv( jenv->GetStringUTFChars(env, NULL), jenv->GetStringUTFChars(value, NULL), over );
 }
 
-DLL_EXPORT void Java_com_valvesoftware_ValveActivity2_nativeOnActivityResult()
+DLL_EXPORT void Java_zzh_source_launcher_utils_GameBridge_nativeOnActivityResult()
 {
 //	Msg( "Java_com_valvesoftware_ValveActivity_nativeOnActivityResult\n" );
 }
 
-DLL_EXPORT void Java_com_valvesoftware_ValveActivity2_setArgs(JNIEnv *env, jclass *clazz, jstring str)
+DLL_EXPORT void Java_zzh_source_launcher_utils_GameBridge_setArgs(JNIEnv *env, jclass *clazz, jstring str)
 {
 	strncpy( java_args, env->GetStringUTFChars(str, NULL), sizeof java_args );
 }
@@ -68,7 +68,7 @@ void SetLauncherArgs()
 	}
 
 	D("-fullscreen");
-	D("-nosteam");
+    D("-nosteam");
 	D("-insecure");
 
 #undef A
@@ -129,6 +129,7 @@ DLL_EXPORT int LauncherMainAndroid( int argc, char **argv )
 	android_property_print("ro.product.model");
 	android_property_print("ro.product.name");
 	android_property_print("Inltlized Args");
+
 	SetLauncherArgs();
 
 	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
