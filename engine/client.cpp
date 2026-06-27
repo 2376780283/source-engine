887c721292bdbf1fb9e66cae5dfb2aa13dd5bb8c
2376780283 <2376780283@qq.com>
2026-06-27 15:52:45 +0800

    添加自定义头像支持

--- engine/client.cpp
+++ engine/client.cpp
@@ -58,6 +58,7 @@ static ConVar cl_timeout( "cl_timeout", "30", FCVAR_ARCHIVE, "After this many se
 	   ConVar cl_logofile( "cl_logofile", "materials/decals/spraylogo.vtf", FCVAR_ARCHIVE, "Spraypoint logo decal." ); // TODO must be more generic
 static ConVar cl_soundfile( "cl_soundfile", "sound/player/jingle.wav", FCVAR_ARCHIVE, "Jingle sound file." );
 static ConVar cl_allowdownload ( "cl_allowdownload", "1", FCVAR_ARCHIVE, "Client downloads customization files" );
+static ConVar cl_avatar( "cl_avatar", "", FCVAR_ARCHIVE, "Custom avatar VTF file path." );
 static ConVar cl_downloadfilter( "cl_downloadfilter", "all", FCVAR_ARCHIVE, "Determines which files can be downloaded from the server (all, none, nosounds, mapsonly)" );
 
 #ifdef OSX
@@ -1231,6 +1232,7 @@ void CClientState::CheckOwnCustomFiles()
 	{
 		AddCustomFile( 0, cl_logofile.GetString() );
 		AddCustomFile( 1, cl_soundfile.GetString() );
+		AddCustomFile( 2, cl_avatar.GetString() );
 	}
 }
 
