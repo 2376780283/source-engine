ecda046af921d3a2b6188177d557cf94c827e044
2376780283 <2376780283@qq.com>
2026-08-14 12:54:27 +0800

    CI:只为android平台编译测试

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
-export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=nh2 --prefix=srceng-mod-launcher/android --android=aarch64,host,21 --target=../aarch64 --enable-opus --togles --disable-warns &&
-./waf install --target=client,server --strip
-
-if [ -e "srceng-mod-launcher/android/lib/arm64-v8a/README.md" ]; then
-	rm srceng-mod-launcher/android/lib/arm64-v8a/README.md
-fi
+export ANDROID_NDK_HOME=$PWD/android-ndk-r28c/
+python3 ./waf configure -T release --prefix=../android_build --android=aarch64,llvm,29 --target=android_build --disable-warns --togles
