bb00f8d42a89072b57db252dc9b78bde48f2b549
KonuriMakiJava <3646441319@qq.com>
2026-08-15 09:22:09 +0800

    vgui(remove): 何意味 extrapanel

--- gameui/BasePanel.h
+++ gameui/BasePanel.h
@@ -15,7 +15,6 @@
 #include "vgui_controls/PHandle.h"
 #include "vgui_controls/MenuItem.h"
 #include "vgui_controls/MessageDialog.h"
-#include "ExtraManagerPanel.h" 
 
 #include "KeyValues.h"
 #include "utlvector.h"
@@ -230,7 +229,6 @@ public:
 	void OnOpenChangeGameDialog();
 	void OnOpenPlayerListDialog();
 	void OnOpenBenchmarkDialog();
-	void ShowExtraManager();
 	void OnOpenOptionsDialog();
 	void OnOpenOptionsDialog_Xbox();
 	void OnOpenLoadCommentaryDialog();
@@ -292,7 +290,6 @@ public:
 	int  GetMenuAlpha( void );
 
 	void SetMainMenuOverride( vgui::VPANEL panel );   
-    ExtraManagerPanel *m_pExtraPanel;
     
 protected:
 	virtual void PaintBackground();
@@ -386,7 +383,6 @@ private:
 	vgui::DHANDLE<vgui::Frame> m_hBenchmarkDialog;
 	vgui::DHANDLE<vgui::Frame> m_hLoadCommentaryDialog;
 	vgui::DHANDLE<vgui::Frame> m_hAchievementsDialog;
-    vgui::DHANDLE<vgui::Frame> m_hExtraDialog;
 
 
 	// Xbox 360
