b1b7ad21e63b5644a012b4fbbbf6a41cbaf5ca66
2376780283 <2376780283@qq.com>
2026-08-17 03:43:43 +0800

    feat(all-SourceApp-Jni): 添加类似SteamContext兼容接口（SourceApp）

--- public/filesystem_init.cpp
+++ public/filesystem_init.cpp
@@ -610,6 +610,29 @@ FSReturnCode_t FileSystem_LoadSearchPaths( CFSSearchPathsInit &initInfo )
         }
     }
 
+	const char *pszExtraPaths = getenv( "EXTRAS_PATH" );
+	if ( pszExtraPaths )
+	{
+		CUtlStringList vecPaths;
+		V_SplitString( pszExtraPaths, ",", vecPaths );
+		FOR_EACH_VEC( vecPaths, idxExtraPath )
+		{
+			char szAbsSearchPath[MAX_PATH];
+			Q_StripPrecedingAndTrailingWhitespace( vecPaths[ idxExtraPath ] );
+			V_MakeAbsolutePath( szAbsSearchPath, sizeof( szAbsSearchPath ), vecPaths[ idxExtraPath ], baseDir );
+			V_FixSlashes( szAbsSearchPath );
+			if ( !V_RemoveDotSlashes( szAbsSearchPath ) )
+			{
+				Warning( "[SourceApp]: Bad extra path - Can't resolve pathname for '%s'\n", szAbsSearchPath );
+				continue;
+			}
+			V_StripTrailingSlash( szAbsSearchPath );
+			FileSystem_AddLoadedSearchPath( initInfo, "GAME", szAbsSearchPath, false );
+			FileSystem_AddLoadedSearchPath( initInfo, "MOD", szAbsSearchPath, false );		
+			Msg( "[SourceApp]: The extra path Mounted: %s\n", szAbsSearchPath );
+		}
+	}
+
 	bool bLowViolence = initInfo.m_bLowViolence;
 	for ( KeyValues *pCur=pSearchPaths->GetFirstValue(); pCur; pCur=pCur->GetNextValue() )
 	{
